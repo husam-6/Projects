c189160c4092947b542d7f5bd8b41f0a1a9379e8
Husam Almanakly <husam@MacBook-Pro.local>
2021-06-27 23:00:39 -0400

    DSA Projects

---
+++ ECE264/Sorting/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<list>
+
+using namespace std;
+
+int main()
+{
+  string bruh = "bruh";
+  string test = "test";
+  string okay = "okay";
+
+  bruh = bruh[0];
+  test = test[0];
+  okay = okay[0];
+
+  string work = bruh + test + okay;
+
+  cout <<  work << '\n';
+
+}
